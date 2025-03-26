6c654d5d761ad7f6861ca7bf56030194be52baef
Keyla <keylareg28@outlook.com>
2025-03-25 22:35:12 -0300

    site de orçamento com formulario

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+
+/*
+variaveis
+espaço de memoria que vc reserva e pode guardar o que quiser la dentro
+3 jeitos de criar uma varial
+-var - jeito antigo
+-let - pode alterar o valor depois
+-const - constante nao altera.
+
+Função = um trecho de codigo que só é executado, quando é chamado*/
+
+const form= document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translatex(0)"
+    mascara.style.visibility = "hidden"
+}
